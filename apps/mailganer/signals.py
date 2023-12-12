ae0102af8feb8d3126c434fb99ea38933191eac4
Jakhongir Sagdullaev <jakhongirs@Jakhongirs-iMac.local>
2023-12-12 15:13:30 +0500

    feat: email template

--- apps/mailganer/signals.py
+++ apps/mailganer/signals.py
@@ -14,12 +14,15 @@ def create_email_distribution_all(sender, instance, created, **kwargs):
             [UserEmailDistribution(user=user, email_distribution=instance) for user in User.objects.all()]
         )
 
-        send_email_task.delay(
-            email=settings.EMAIL_HOST_USER,
-            subject=instance.template.name,
-            message=instance.template.template,
-            recipient_list=[user.email for user in User.objects.all()],
-        )
+        for user in User.objects.all():
+            send_email_task.delay(
+                email=settings.EMAIL_HOST_USER,
+                subject=instance.template.name,
+                message=instance.template.template,
+                recipient_list=[user.email],
+                user_fullname=user.full_name,
+                user_birthdate=user.birth_date.strftime("%Y-%m-%d"),  # Replace with your actual method to get birthdate
+            )
 
 
 @receiver(m2m_changed, sender=EmailDistribution.users.through)
@@ -32,9 +35,12 @@ def create_user_email_distribution(sender, instance, action, **kwargs):
                 [UserEmailDistribution(user=user, email_distribution=instance) for user in instance.users.all()]
             )
 
-            send_email_task.delay(
-                email=settings.EMAIL_HOST_USER,
-                subject=instance.template.name,
-                message=instance.template.template,
-                recipient_list=[user.email for user in instance.users.all()],
-            )
+            for user in instance.users.all():
+                send_email_task.delay(
+                    email=settings.EMAIL_HOST_USER,
+                    subject=instance.template.name,
+                    message=instance.template.template,
+                    recipient_list=[user.email],
+                    user_fullname=user.full_name,
+                    user_birthdate=user.birth_date.strftime("%Y-%m-%d"),
+                )
--- apps/mailganer/tasks.py
+++ apps/mailganer/tasks.py
@@ -1,7 +1,13 @@
 from celery import shared_task
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 
 
 @shared_task
-def send_email_task(email, subject, message, recipient_list):
-    send_mail(subject, message, email, recipient_list, fail_silently=False)
+def send_email_task(email, subject, message, recipient_list, user_fullname, user_birthdate):
+    # Replace placeholders with actual values
+    message = message.replace("{{ user_fullname }}", user_fullname)
+    message = message.replace("{{ user_birthdate }}", user_birthdate)
+
+    email_message = EmailMessage(subject, message, email, recipient_list)
+    email_message.content_subtype = "html"  # Set the content type to HTML
+    email_message.send()
