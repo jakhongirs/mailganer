f41a13d8182162087a0c5c846ca93cc824d602c2
jakhongirs <jakhongirsv@gmail.com>
2023-12-12 16:41:12 +0500

    fix: variables in email

--- apps/mailganer/tasks.py
+++ apps/mailganer/tasks.py
@@ -7,15 +7,12 @@ def send_email_task(email, subject, message, recipient_list, user_fullname, user
     user_fullname = user_fullname if user_fullname else ""
     user_birthdate = user_birthdate.strftime("%d.%m.%Y") if user_birthdate else ""
 
+    message = message.replace("{{ user_fullname }}", user_fullname)
+    message = message.replace("{{ user_birthdate }}", user_birthdate)
+
     # Appending tracking pixel to the message
     tracking_pixel = f'<img src="{tracking_pixel_url}" width="1" height="1" alt="tracking pixel"/>'
-    message = f"{message}{tracking_pixel}"
-
-    # Replacing the placeholders with the actual values
-    message = message.replace("{{user_fullname}}", user_fullname)
-    message = message.replace("{{user_birthdate}}", user_birthdate)
-
-    print(message)
+    message = f"{message} {tracking_pixel}"
 
     email_message = EmailMessage(subject, message, email, recipient_list)
     email_message.content_subtype = "html"
