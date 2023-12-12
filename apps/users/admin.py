24b69fb394fe1bbe9c6bdbdbef3358219f06ec79
Jakhongir Sagdullaev <jakhongirs@Jakhongirs-iMac.local>
2023-12-12 13:51:27 +0500

    feat: user admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from apps.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "full_name", "phone_number", "email")
+    search_fields = ("full_name", "phone_number", "email")
